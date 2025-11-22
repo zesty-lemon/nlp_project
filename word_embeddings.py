b657d9a53254fb1d992fa8a308daf624b58991ac
Giles <51659929+zesty-lemon@users.noreply.github.com>
2025-11-22 11:31:47 -0500

    word embeddings skeleton

--- word_embeddings.py
+++ word_embeddings.py
@@ -1,5 +1,8 @@
+import numpy as np
 import torch
-from transformers import BertModel, BertTokenizer
+from sklearn.decomposition import PCA
+from tqdm import tqdm
+from transformers import BertModel, BertTokenizer, AutoTokenizer, AutoModel
 
 # Load pre - trained tokenizer
 tokenizer = BertTokenizer.from_pretrained("bert - base - uncased")
@@ -15,3 +18,23 @@ def get_embedding(text):
         outputs = model(**inputs)
     # Use [CLS] token embedding as sentence representation
     return outputs.last_hidden_state[0][0].numpy()  # [CLS] token
+
+
+# perform and return bert embeddings
+# performed on whole episodes worth of dialogs at once
+def perform_bert_embedding(dialogs: list[str]) -> list[np.ndarray]:
+    embeddings = []
+
+    for poem in tqdm(range(0, len(dialogs)), desc="Performing BERT Embedding", unit="poem", colour="blue"):
+        bert_embedding = get_embedding(dialogs[poem])
+        embeddings.append(bert_embedding)
+
+    return embeddings
+
+
+# perform PCA and return principle vectors on BERT vectors
+def perform_pca(bert_vectors: list[np.ndarray], num_components = 2) -> np.ndarray:
+    arr_bert_vectors = np.vstack(bert_vectors)
+    pca = PCA(n_components=num_components)
+    bert_pca = pca.fit_transform(arr_bert_vectors)
+    return bert_pca
