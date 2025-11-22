b9699d8ba46a795609f0c52af669d5000518161f
Giles <51659929+zesty-lemon@users.noreply.github.com>
2025-11-22 12:01:00 -0500

    print lines

--- corpus_utils.py
+++ corpus_utils.py
@@ -109,8 +109,10 @@ def get_everything():
             df_dialogs = get_all_dialogs_from_episode_df(episode_dialogs)
 
             # Concat dataframe to overall show dataframe
+            print(f"Len Before Adding {episode_filepath} : {len(full_corpus_df)}")
             full_corpus_df = pd.concat([full_corpus_df, df_dialogs], axis=0)
-
+            print(f"Len After Adding {episode_filepath} : {len(full_corpus_df)}")
+    print(f"Final Full Corpus Length : {len(full_corpus_df)}")
     print(full_corpus_df.head())
 
 
