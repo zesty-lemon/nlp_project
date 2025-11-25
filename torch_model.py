7b07043c04260bbcf58efa5dd3a39620add61243
trueshiba <107582031+trueshiba@users.noreply.github.com>
2025-11-25 11:51:18 -0500

    pseudo code

--- torch_model.py
+++ torch_model.py
@@ -1,6 +1,6 @@
 import torch
 
-# Check if CUDA is available
+# Check if CUDA is available. CUDA will not be availalble on Mac.
 cuda_available = torch.cuda.is_available()
 print(f"CUDA available: {cuda_available}")
 
@@ -12,3 +12,19 @@ if cuda_available:
     # Get the name of the first CUDA device
     device_name = torch.cuda.get_device_name(0)
     print(f"Device name: {device_name}")
+
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+print(f"Using device: {device}")
+
+# Load vector embeddings from embeddings.npy file
+
+# Split the embeddings into train test split validation sets
+
+# Create the model
+
+# Train for n epochs on the train and test data
+
+# Check final validation accuracy on validation data
