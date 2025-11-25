691320ce2684ec95df513ed6eb85bf684d3641d5
Giles <51659929+zesty-lemon@users.noreply.github.com>
2025-11-25 12:54:21 -0500

    multiple embedding choices

--- constants.py
+++ constants.py
@@ -1,13 +1,14 @@
 """
 Reusable Constants File
 """
-
+from enum import Enum
 from pathlib import Path
 
 # File Operations
 BIG_BANG_THEORY_DIR = Path("data/big_bang_theory")
 NUM_SEASONS = 5
-SAVED_EMBEDDINGS_DIR = Path("output/embeddings.npy")
+BERT_EMBEDDINGS_DIR = Path("output/bert_embeddings.npy")
+S_BERT_EMBEDDINGS_DIR = Path("output/s_bert_embeddings.npy")
 
 # Runtime Parameters
 NUM_DIALOG_TURNS = 5
@@ -19,3 +20,9 @@ SEPARATOR = "<s>"
 # ground truth labels: Humour = 1, Non-Humour = 0
 GT_HUMOUR = 1
 GT_NON_HUMOUR = 0
+
+# enum to store different BERT model choices
+# value = output directory of saved embeddings
+class BERT_MODEL(Enum):
+    BERT = "output/bert_embeddings.npy"
+    S_BERT = "output/s_bert_embeddings.npy"
