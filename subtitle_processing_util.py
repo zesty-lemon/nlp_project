b93a2aebec7d6e9417a9155c21f10a86b2bf3d04
Giles <51659929+zesty-lemon@users.noreply.github.com>
2025-12-03 22:11:38 -0500

    basic subtitle parsing

--- subtitle_processing_util.py
+++ subtitle_processing_util.py
@@ -1,7 +1,5 @@
 import re
 
-
-
 # Strip Timestamps
 def strip_header_footer(text:str) -> str:
     srt_pattern = r'^\d+\n\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},\d{3}$'
@@ -19,18 +17,26 @@ if __name__ == "__main__":
 
     episode_lines = episode.splitlines()
 
-    cleaned_episode = []
-    for i in range(0, len(episode_lines)):
-        if not episode_lines[i].endswith((".", "!", "?")):
-            if i+1 <= len(episode_lines):
-                cleaned_episode.append(episode_lines[i]+episode_lines[i+1])
-            else:
-                cleaned_episode.append(episode_lines[i])
-        else:
-            pass
+# if the next one is not an empty string, loop until len(episode_lines).  Loop till you find the next one
+# then concatonate i (original) to index(after search) and increment i
 
-    processed_episode = "\n".join(cleaned_episode)
+    # # deos not wor, going 2 bed
+    # cleaned_episode = []
+    # for i in range(0, len(episode_lines)-1):
+    #     if (episode_lines[i+1] != ""):
+    #         if i+1 <= len(episode_lines): # just in case
+    #             for j in range(i+1, len(episode_lines) - 1):
+    #
+    #         else:
+    #             cleaned_episode.append(episode_lines[i])
+    #
+    #         else:
+    #             cleaned_episode.append(episode_lines[i])
+    #     else:
+    #         pass
 
+    # processed_episode = "\n".join(cleaned_episode)
+    # print(processed_episode)
     with open("data/subtitles/young_sheldon_s4_e16_cleaned.txt", "w", encoding="utf-8") as file:
         file.write(episode)
 
